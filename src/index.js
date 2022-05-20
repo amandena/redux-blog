1aa664705d76d37726d1f21c1497e171d7a196b7
amandena <manda.m.johnson@gmail.com>
2022-05-19 22:04:37 -0400

    wrapped app in provider with store and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Provider } from 'react-redux'
+import { legacy_createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './components/App'
+import reducers from './reducers'
+
+const store = legacy_createStore(reducers, applyMiddleware(thunk))
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
